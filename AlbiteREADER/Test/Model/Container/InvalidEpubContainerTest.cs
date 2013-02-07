4ab2df56582f5901abea5892bb313624480ea1e8
Svetlin Ankov <galileostudios@gmail.com>
2013-02-08 01:17:01 +0200

    Added InvalidEpubContainerTest
    It asserts that an invalid epub would fail to open

---
+++ AlbiteREADER/Test/Model/Container/InvalidEpubContainerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SvetlinAnkov.AlbiteREADER.Test.Model.Container
+{
+    public class InvalidEpubContainerTest : EpubContainerTest
+    {
+        /// <summary>
+        /// Test an invalid epub container.
+        /// The test will fail if the epub container doesn't throw an exception
+        /// </summary>
+        /// <param name="epubPath">Path to the epub resource</param>
+        public InvalidEpubContainerTest(string epubPath) : base(epubPath) { }
+
+        protected override void TestImplementation()
+        {
+            try
+            {
+                base.TestImplementation();
+            }
+            catch (Exception e)
+            {
+                // All is fine, there was an error.
+                Log("Successfully threw an exception: " + e.Message
+                    + (e.InnerException != null ? " -> " + e.InnerException.Message : ""));
+                return;
+            }
+
+            throw new Exception("The test didn't throw an exception when expected to");
+        }
+    }
+}
