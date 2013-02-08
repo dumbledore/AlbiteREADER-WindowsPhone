a0c48e2e47783c2970963ed5e17896c94e95b30c
Svetlin Ankov <galileostudios@gmail.com>
2013-02-08 21:55:27 +0200

    Abstracted the tests a bit more

---
+++ AlbiteREADER/Test/TemplateTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SvetlinAnkov.AlbiteREADER.Test.Layout;
+using SvetlinAnkov.AlbiteREADER.Utils;
+
+namespace SvetlinAnkov.AlbiteREADER.Test
+{
+    public class TemplateTests : TestCase
+    {
+        protected override void TestImplementation()
+        {
+            new TemplateTest(Defaults.Engine.LayoutPath + Defaults.Engine.MainPage).Test();
+            new TemplateTest(Defaults.Engine.LayoutPath + Defaults.Engine.BaseStyles).Test();
+            new TemplateTest(Defaults.Engine.LayoutPath + Defaults.Engine.ContentStyles).Test();
+            new TemplateTest(Defaults.Engine.LayoutPath + Defaults.Engine.ThemeStyles).Test();
+        }
+    }
+}
---
+++ AlbiteREADER/Test/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SvetlinAnkov.AlbiteREADER.Test.Layout;
+
+namespace SvetlinAnkov.AlbiteREADER.Test
+{
+    public class Tests : TestCase
+    {
+        protected override void TestImplementation()
+        {
+            new EpubTests().Test();
+            new TemplateTests().Test();
+            new EngineTest().Test();
+        }
+    }
+}
