8a3006d79d1d1952da6e6c5ef77fe60d195d22f7
Svetlin Ankov <galileostudios@gmail.com>
2013-03-16 13:23:12 +0200

    Modified the copy-store script to fetch the data for both AlbiteREADER and AlbiteTests

--- copy-store.bat
+++ copy-store.bat
@@ -1,1 +1,5 @@
-"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd bdf349d4-69ac-4450-a13f-ca351b98353c c:\temp\wp7
+REM Albite READER
+"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd bdf349d4-69ac-4450-a13f-ca351b98353c c:\temp\wp7\AlbiteREADER
+
+REM Albite Tests
+"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd 9b45c86a-6522-4d1c-8cd8-3d79d377b8d9 c:\temp\wp7\AlbiteTests
