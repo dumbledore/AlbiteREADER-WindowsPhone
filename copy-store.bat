fcf1d9ea03865b4c8af062d9d53083cd1fdf29ad
Svetlin Ankov <galileostudios@gmail.com>
2013-03-17 10:27:15 +0200

    Updated the copy script to first delete the directories.

--- copy-store.bat
+++ copy-store.bat
@@ -1,5 +1,7 @@
-REM Albite READER
-"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd bdf349d4-69ac-4450-a13f-ca351b98353c c:\temp\wp7\AlbiteREADER
+@echo Albite READER
+@rmdir /s /q c:\temp\wp7\AlbiteREADER
+@"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd bdf349d4-69ac-4450-a13f-ca351b98353c c:\temp\wp7\AlbiteREADER
 
-REM Albite Tests
-"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd 9b45c86a-6522-4d1c-8cd8-3d79d377b8d9 c:\temp\wp7\AlbiteTests
+@echo Albite Tests
+@rmdir /s /q c:\temp\wp7\AlbiteTests
+@"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd 9b45c86a-6522-4d1c-8cd8-3d79d377b8d9 c:\temp\wp7\AlbiteTests
