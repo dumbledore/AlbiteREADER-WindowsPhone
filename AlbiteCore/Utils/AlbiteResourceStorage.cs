058c4de8366c0c9fd1ac484b798f53c60fa1d9d3
Svetlin Ankov <galileostudios@gmail.com>
2013-04-07 14:28:09 +0300

    AlbiteResourceStorage: use Path.Combine()

--- AlbiteCore/Utils/AlbiteResourceStorage.cs
+++ AlbiteCore/Utils/AlbiteResourceStorage.cs
@@ -2,12 +2,6 @@
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Windows.Resources;
 using System.IO;
 
@@ -15,13 +9,14 @@ namespace SvetlinAnkov.Albite.Core.Utils
 {
     public class AlbiteResourceStorage : AlbiteStorage
     {
-        private static readonly string ResourcesLocation = "Resources/";
+        private static readonly string ResourcesLocation = "Resources";
 
         public AlbiteResourceStorage(string filename) : base(filename) { }
 
         public override Stream GetStream(FileAccess access, FileMode mode, FileShare share)
         {
-            StreamResourceInfo sr = Application.GetResourceStream(new Uri(ResourcesLocation + FileName, UriKind.Relative));
+            StreamResourceInfo sr = Application.GetResourceStream(
+                new Uri(Path.Combine(ResourcesLocation, FileName), UriKind.Relative));
 
             if (sr == null)
             {
