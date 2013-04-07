f78528b6b83af3507ba574a09f98c1e21ae442c8
Svetlin Ankov <galileostudios@gmail.com>
2013-04-07 14:34:28 +0300

    Stripped the Chapter class from the bloat

--- AlbiteREADER/Model/Chapter.cs
+++ AlbiteREADER/Model/Chapter.cs
@@ -2,28 +2,22 @@
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using SvetlinAnkov.Albite.READER.Layout;
 
 namespace SvetlinAnkov.Albite.READER.Model
 {
+
+    /// <summary>
+    /// An XHTML resource from a book.
+    /// </summary>
     public class Chapter
     {
-        public Chapter PreviousChapter { get; private set; }
-        public Chapter NextChapter {get; private set; }
         public Book Book { get; private set; }
-        public Location ReadingLocation { get; set; }
 
-        public Chapter(Book book, Chapter previous, Chapter next)
+        public Chapter()
         {
-            Book = book;
-            PreviousChapter = previous;
-            NextChapter = next;
+
         }
+
+        // TODO: Chapter Bookmarks, Highlights and Notes
     }
 }
