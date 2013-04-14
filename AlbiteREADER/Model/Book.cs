45ab0f2b30986107965e386c8cdee530b8ac9a7c
Svetlin Ankov <galileostudios@gmail.com>
2013-04-14 14:10:35 +0300

    Book: Tidied up the imports

--- AlbiteREADER/Model/Book.cs
+++ AlbiteREADER/Model/Book.cs
@@ -2,16 +2,12 @@
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
-using SvetlinAnkov.Albite.Core.Utils;
-using SvetlinAnkov.Albite.READER.Model.Container;
 using System.Linq;
 using System.Data.Linq.Mapping;
+using System.Data.Linq;
+using System.Collections.Generic;
+using SvetlinAnkov.Albite.Core.Utils;
+using SvetlinAnkov.Albite.READER.Model.Container;
 using SvetlinAnkov.Albite.READER.BrowserEngine;
 
 namespace SvetlinAnkov.Albite.READER.Model
