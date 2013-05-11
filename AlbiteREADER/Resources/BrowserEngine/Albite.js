ac050b7190c95706e0b82f354f2520360a4ef03e
Svetlin Ankov <galileostudios@gmail.com>
2013-05-11 11:54:29 +0300

    Albite.JS: Added a sleep function for debugging purposes

--- AlbiteREADER/Resources/BrowserEngine/Albite.js
+++ AlbiteREADER/Resources/BrowserEngine/Albite.js
@@ -65,6 +65,14 @@ function Albite(mainWindow, pageWidth, currentPageNumber, debugEnabled) {
         }
     }
 
+    function sleep(millis)
+    {
+        var date = new Date();
+        var curDate = null;
+        do { curDate = new Date(); }
+        while(curDate - date < millis);
+    }
+
     function handleError(msg, url, line) {
         log(msg + " on line " + line + " for " + url, true);
     }
