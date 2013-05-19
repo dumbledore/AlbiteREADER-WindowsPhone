ac3772271c2377aaa3eb6dedb0b2a787458d098e
Svetlin Ankov <galileostudios@gmail.com>
2013-05-19 17:43:39 +0300

    Hide the the FPS in debug

--- Tests/App.xaml.cs
+++ Tests/App.xaml.cs
@@ -77,7 +77,7 @@ namespace SvetlinAnkov.Albite.Tests
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 // Display the current frame rate counters.
-                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+                //Application.Current.Host.Settings.EnableFrameRateCounter = true;
 
                 // Show the areas of the app that are being redrawn in each frame.
                 //Application.Current.Host.Settings.EnableRedrawRegions = true;
