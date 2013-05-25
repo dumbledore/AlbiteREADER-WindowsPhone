89ccd174a2b6bd28b179059b6ae085dd817cf326
Svetlin Ankov <galileostudios@gmail.com>
2013-05-25 10:07:49 +0300

    copy-store.bat now copies the files locally

--- copy-store.bat
+++ copy-store.bat
@@ -1,7 +1,7 @@
 @echo Albite READER
-@rmdir /s /q c:\temp\wp7\AlbiteREADER
-@"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd bdf349d4-69ac-4450-a13f-ca351b98353c c:\temp\wp7\AlbiteREADER
+@rmdir /s /q IsoTemp\AlbiteREADER
+@"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd bdf349d4-69ac-4450-a13f-ca351b98353c IsoTemp\AlbiteREADER
 
 @echo Albite Tests
-@rmdir /s /q c:\temp\wp7\AlbiteTests
-@"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd 9b45c86a-6522-4d1c-8cd8-3d79d377b8d9 c:\temp\wp7\AlbiteTests
+@rmdir /s /q IsoTemp\AlbiteTests
+@"c:\Program Files (x86)\Microsoft SDKs\Windows Phone\v7.1\Tools\IsolatedStorageExplorerTool\ISETool.exe" ts xd 9b45c86a-6522-4d1c-8cd8-3d79d377b8d9 IsoTemp\AlbiteTests
