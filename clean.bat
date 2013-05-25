50e5f7df72b0df12519b9409842979cfafe333b0
Svetlin Ankov <galileostudios@gmail.com>
2013-05-25 10:08:16 +0300

    Added clean.bat

---
+++ clean.bat
@@ -0,0 +1,10 @@
+@echo Cleaning Build Data...
+@rmdir /s /q AlbiteCore\Bin
+@rmdir /s /q AlbiteCore\obj
+@rmdir /s /q AlbiteREADER\Bin
+@rmdir /s /q AlbiteREADER\obj
+@rmdir /s /q Tests\Bin
+@rmdir /s /q Tests\obj
+
+@echo Cleaning Storage Temp Data...
+@rmdir /s /q IsoTemp\
