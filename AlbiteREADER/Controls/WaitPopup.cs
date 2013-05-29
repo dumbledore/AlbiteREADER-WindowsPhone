d89cb2beeb429c808af6edb4647854cb85cf8ff6
Svetlin Ankov <galileostudios@gmail.com>
2013-05-30 00:08:47 +0300

    WaitPopup: Hide the bars when showing it
    This might cause a couple of re-layouts if the bars are fully
    opaque. Therefore, it rather assumes that they are not, i.e. their
    opacity < 1.0

--- AlbiteREADER/Controls/WaitPopup.cs
+++ AlbiteREADER/Controls/WaitPopup.cs
@@ -9,6 +9,9 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Controls.Primitives;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.ComponentModel;
 
 namespace SvetlinAnkov.Albite.READER.Controls
 {
@@ -32,7 +35,32 @@ namespace SvetlinAnkov.Albite.READER.Controls
         public bool IsOpen
         {
             get { return popup.IsOpen; }
-            set { popup.IsOpen = value; }
+            set
+            {
+                hideBars();
+                popup.IsOpen = value;
+            }
+        }
+
+        private void hideBars()
+        {
+            if (DesignerProperties.IsInDesignTool)
+            {
+                // Setting SystemTray.IsVisible causes exceptions in design mode,
+                // so skipping it, if used in VS, Expression Blend, etc.
+                // For more info, check: http://stackoverflow.com/a/834332/348183
+                return;
+            }
+
+            // Hide the top bar
+            SystemTray.IsVisible = false;
+
+            // Hide the bottom bar
+            IApplicationBar bar = AppUtils.ApplicationBar;
+            if (bar != null)
+            {
+                bar.IsVisible = false;
+            }
         }
     }
 }
