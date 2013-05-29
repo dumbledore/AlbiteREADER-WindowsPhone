2e219dc3e74b51851e29be9f4665a8bb3ca8b0cd
Svetlin Ankov <galileostudios@gmail.com>
2013-05-30 00:07:20 +0300

    Added AppUtils

---
+++ AlbiteREADER/AppUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace SvetlinAnkov.Albite.READER
+{
+    public class AppUtils
+    {
+        public static PhoneApplicationPage CurrentPage
+        {
+            get
+            {
+                Application current = Application.Current;
+                if (current == null)
+                {
+                    return null;
+                }
+
+                PhoneApplicationFrame frame = current.RootVisual as PhoneApplicationFrame;
+                if (frame == null)
+                {
+                    return null;
+                }
+
+                return frame.Content as PhoneApplicationPage;
+            }
+        }
+
+        public static IApplicationBar ApplicationBar
+        {
+            get
+            {
+                PhoneApplicationPage currentPage = CurrentPage;
+                if (currentPage == null)
+                {
+                    return null;
+                }
+
+                return currentPage.ApplicationBar;
+            }
+        }
+    }
+}
