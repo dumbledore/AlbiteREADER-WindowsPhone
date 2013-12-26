5da7ff87bdafa7549471736b7658308dd3ca88a6
Svetlin Ankov <galileostudios@gmail.com>
2013-12-26 22:58:11 +0200

    ThemeSettingsPage: Current theme is now selected

--- AlbiteREADER/Source/View/Pages/BookSettings/ThemeSettingsPage.xaml.cs
+++ AlbiteREADER/Source/View/Pages/BookSettings/ThemeSettingsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Phone.Controls;
 using SvetlinAnkov.Albite.Engine.LayoutSettings;
 using SvetlinAnkov.Albite.READER.View.Controls;
+using System.Windows;
 using System.Windows.Input;
 using GEArgs = System.Windows.Input.GestureEventArgs;
 
@@ -11,6 +12,30 @@ namespace SvetlinAnkov.Albite.READER.View.Pages.BookSettings
         public ThemeSettingsPage()
         {
             InitializeComponent();
+            setDefaultItem();
+        }
+
+        private void setDefaultItem()
+        {
+            // Get the context
+            AlbiteContext context = ((IAlbiteApplication)App.Current).CurrentContext;
+
+            // Get current layout settings
+            Settings settings = context.Settings;
+
+            // Get current theme
+            Theme theme = settings.Theme;
+
+            foreach (UIElement element in ContentList.Children)
+            {
+                ThemeControl control = (ThemeControl)element;
+                if (control.BackgroundColor == theme.BackgroundColor
+                    && control.ForegroundColor == theme.TextColor)
+                {
+                    control.Selected = true;
+                    break;
+                }
+            }
         }
 
         private void ThemeControl_Tap(object sender, GEArgs e)
