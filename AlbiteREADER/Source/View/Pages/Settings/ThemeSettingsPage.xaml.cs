4af142f06c73d856850daf33033b6ce77f3082dc
Svetlin Ankov <galileostudios@gmail.com>
2013-12-26 22:47:23 +0200

    ThemeSettings are now editeable

--- AlbiteREADER/Source/View/Pages/Settings/ThemeSettingsPage.xaml.cs
+++ AlbiteREADER/Source/View/Pages/Settings/ThemeSettingsPage.xaml.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
+﻿using Microsoft.Phone.Controls;
+using SvetlinAnkov.Albite.READER.View.Controls;
+using System.Windows.Input;
+using GEArgs = System.Windows.Input.GestureEventArgs;
 
 namespace SvetlinAnkov.Albite.READER.View.Pages.Settings
 {
@@ -15,6 +10,28 @@ namespace SvetlinAnkov.Albite.READER.View.Pages.Settings
         public ThemeSettingsPage()
         {
             InitializeComponent();
+        }
+
+        private void ThemeControl_Tap(object sender, GEArgs e)
+        {
+            ThemeControl control = (ThemeControl)sender;
+
+            // Get the context
+            AlbiteContext context = ((IAlbiteApplication)App.Current).CurrentContext;
+
+            // Get current layout settings
+            SvetlinAnkov.Albite.Engine.LayoutSettings.Settings settings = context.Settings;
+
+            // Update the theme
+            settings.Theme = new SvetlinAnkov.Albite.Engine.LayoutSettings.Theme(
+                control.ForegroundColor,
+                control.BackgroundColor);
+
+            // Update & persist
+            context.Settings = settings;
+
+            // Go back
+            NavigationService.GoBack();
         }
     }
 }
