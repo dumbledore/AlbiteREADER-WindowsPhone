641cd1365415c7284d9734fca8a205b69f3387b0
Svetlin Ankov <galileostudios@gmail.com>
2013-12-27 21:40:39 +0200

    FontFamilySettingsPage is fully functional

--- AlbiteREADER/Source/View/Pages/BookSettings/FontFamilySettingsPage.xaml.cs
+++ AlbiteREADER/Source/View/Pages/BookSettings/FontFamilySettingsPage.xaml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using Microsoft.Phone.Controls;
+using SvetlinAnkov.Albite.Engine.LayoutSettings;
+using SvetlinAnkov.Albite.READER.View.Controls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
+using GEArgs = System.Windows.Input.GestureEventArgs;
 
 namespace SvetlinAnkov.Albite.READER.View.Pages.BookSettings
 {
@@ -15,6 +11,49 @@ namespace SvetlinAnkov.Albite.READER.View.Pages.BookSettings
         public FontFamilySettingsPage()
         {
             InitializeComponent();
+            setDefaultItem();
+        }
+
+        private void setDefaultItem()
+        {
+            // Get the context
+            AlbiteContext context = ((IAlbiteApplication)App.Current).CurrentContext;
+
+            // Get current layout settings
+            Settings settings = context.Settings;
+
+            // Get current font family
+            string fontFamily = settings.FontSettings.Family;
+
+            foreach (UIElement element in ContentList.Children)
+            {
+                ThemeControl control = (ThemeControl)element;
+                if (fontFamily.Equals(control.FontFamily.Source, System.StringComparison.InvariantCultureIgnoreCase))
+                {
+                    control.Selected = true;
+                    break;
+                }
+            }
+        }
+
+        private void ThemeControl_Tap(object sender, GEArgs e)
+        {
+            ThemeControl control = (ThemeControl)sender;
+
+            // Get the context
+            AlbiteContext context = ((IAlbiteApplication)App.Current).CurrentContext;
+
+            // Get current layout settings
+            Settings settings = context.Settings;
+
+            // Update the theme
+            settings.FontSettings.Family = control.FontFamily.Source;
+
+            // Update & persist
+            context.Settings = settings;
+
+            // Go back
+            NavigationService.GoBack();
         }
     }
 }
