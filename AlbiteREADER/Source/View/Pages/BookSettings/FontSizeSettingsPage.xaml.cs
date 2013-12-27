dae38d7236d15017b7a38a97b14916d9a14fdb26
Svetlin Ankov <galileostudios@gmail.com>
2013-12-27 23:14:22 +0200

    FontSizeSettingsPage is done

--- AlbiteREADER/Source/View/Pages/BookSettings/FontSizeSettingsPage.xaml.cs
+++ AlbiteREADER/Source/View/Pages/BookSettings/FontSizeSettingsPage.xaml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using Microsoft.Phone.Controls;
+using SvetlinAnkov.Albite.Engine.Layout;
+using SvetlinAnkov.Albite.READER.View.Controls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
+using GEArgs = System.Windows.Input.GestureEventArgs;
 
 namespace SvetlinAnkov.Albite.READER.View.Pages.BookSettings
 {
@@ -15,6 +11,61 @@ namespace SvetlinAnkov.Albite.READER.View.Pages.BookSettings
         public FontSizeSettingsPage()
         {
             InitializeComponent();
+            setDefaultItem();
+        }
+
+        private void setDefaultItem()
+        {
+            // Get the context
+            AlbiteContext context = ((IAlbiteApplication)App.Current).CurrentContext;
+
+            // Get current layout settings
+            LayoutSettings settings = context.LayoutSettings;
+
+            // Get current font family
+            int fontSize = settings.FontSettings.FontSize.Size;
+
+            foreach (UIElement element in ContentList.Children)
+            {
+                ThemeControl control = (ThemeControl)element;
+                if ((int) control.FontSize == fontSize)
+                {
+                    control.Selected = true;
+                    break;
+                }
+            }
+        }
+
+        private void ThemeControl_Tap(object sender, GEArgs e)
+        {
+            ThemeControl control = (ThemeControl)sender;
+
+            // Get the context
+            AlbiteContext context = ((IAlbiteApplication)App.Current).CurrentContext;
+
+            // Get current layout settings
+            LayoutSettings settings = context.LayoutSettings;
+
+            // Update the font size
+            FontSettings fontSettings = new FontSettings(
+                settings.FontSettings.Family,
+                new FontSize(
+                    control.Text,
+                    (int)control.FontSize));
+
+            // Create the new settings
+            LayoutSettings newSettings
+                = new LayoutSettings(
+                    fontSettings,
+                    settings.TextSettings,
+                    settings.MarginSettings,
+                    settings.Theme);
+
+            // Update & persist
+            context.LayoutSettings = newSettings;
+
+            // Go back
+            NavigationService.GoBack();
         }
     }
 }
