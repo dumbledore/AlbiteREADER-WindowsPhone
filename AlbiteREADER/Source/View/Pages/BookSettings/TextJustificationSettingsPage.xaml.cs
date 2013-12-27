fe62b6d0d882f1acb57fdee21522efa506b5d1ee
Svetlin Ankov <galileostudios@gmail.com>
2013-12-27 23:25:55 +0200

    TextJustificationSettingsPage is done

--- AlbiteREADER/Source/View/Pages/BookSettings/TextJustificationSettingsPage.xaml.cs
+++ AlbiteREADER/Source/View/Pages/BookSettings/TextJustificationSettingsPage.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
+﻿using Microsoft.Phone.Controls;
+using SvetlinAnkov.Albite.Engine.Layout;
 using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
 
 namespace SvetlinAnkov.Albite.READER.View.Pages.BookSettings
 {
@@ -15,6 +9,63 @@ namespace SvetlinAnkov.Albite.READER.View.Pages.BookSettings
         public TextJustificationSettingsPage()
         {
             InitializeComponent();
+        }
+
+        private void setCurrentState()
+        {
+            // Get the context
+            AlbiteContext context = ((IAlbiteApplication)App.Current).CurrentContext;
+
+            // Get current layout settings
+            LayoutSettings settings = context.LayoutSettings;
+
+            // Set justification state
+            JustificationSwitch.IsChecked = settings.TextSettings.Justified;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Set the current state
+            setCurrentState();
+
+            // Go on as usual
+            base.OnNavigatedTo(e);
+        }
+
+        private void applySettings()
+        {
+            // Get the context
+            AlbiteContext context = ((IAlbiteApplication)App.Current).CurrentContext;
+
+            // Get current layout settings
+            LayoutSettings settings = context.LayoutSettings;
+
+            // Update the justification state
+            bool? justified = JustificationSwitch.IsChecked;
+
+            TextSettings textSettings = new TextSettings(
+                settings.TextSettings.LineHeight,
+                justified.HasValue ? justified.Value : false);
+
+            // Create the new settings
+            LayoutSettings newSettings
+                = new LayoutSettings(
+                    settings.FontSettings,
+                    textSettings,
+                    settings.MarginSettings,
+                    settings.Theme);
+
+            // Update & persist
+            context.LayoutSettings = newSettings;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            // First apply the new settings
+            applySettings();
+
+            // Go on as usual
+            base.OnNavigatingFrom(e);
         }
     }
 }
