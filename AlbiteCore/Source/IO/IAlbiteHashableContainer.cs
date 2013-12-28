22a7e5bfe8b1d7ffda85baa684b9c1640c1d2431
Svetlin Ankov <galileostudios@gmail.com>
2013-12-28 21:55:17 +0200

    Added IAlbiteHashableContainer

---
+++ AlbiteCore/Source/IO/IAlbiteHashableContainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+
+namespace SvetlinAnkov.Albite.Core.IO
+{
+    public interface IAlbiteHashableContainer : IAlbiteContainer
+    {
+        // Hash of all container's data
+        byte[] ComputeHash(HashAlgorithm hashAlgorithm);
+    }
+}
