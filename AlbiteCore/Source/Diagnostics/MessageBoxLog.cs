a6966622fc4a3c626cf2ef2bac307544427395dc
Svetlin Ankov <galileostudios@gmail.com>
2014-01-04 22:51:12 +0200

    Added MessageBoxLog

---
+++ AlbiteCore/Source/Diagnostics/MessageBoxLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace SvetlinAnkov.Albite.Core.Diagnostics
+{
+    internal class MessageBoxLog : AbstractLog
+    {
+        public override void Log(Level level, string tag, string message, Exception exception)
+        {
+            // Show only errors to the user
+            if (level == Level.Error)
+            {
+                string errorMessage = message;
+
+                if (exception != null)
+                {
+                    // Add info from exception if there's any
+                    errorMessage = string.Format("{0}\n\n{1}\n\n{2}", message, exception.Message, exception.StackTrace);
+                }
+
+                MessageBox.Show(errorMessage, "An error has occurred", MessageBoxButton.OK);
+            }
+        }
+
+        public override void Flush() { }
+
+        public override void Dispose() { }
+    }
+}
