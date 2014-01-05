7b54bd6fdd610bbe4f442ac7322d14af38430d7a
Svetlin Ankov <galileostudios@gmail.com>
2014-01-05 16:24:45 +0200

    Added CircularBufferQueue

---
+++ AlbiteCore/Source/Collections/CircularBufferQueue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace SvetlinAnkov.Albite.Core.Collections
+{
+    public class CircularBufferQueue<TValue> : CircularBuffer<TValue>
+    {
+        public CircularBufferQueue(int maximumCapacity)
+            : base(maximumCapacity) { }
+
+        public CircularBufferQueue(IEnumerable<TValue> collection)
+            : base(collection) { }
+
+        public void Queue(TValue item)
+        {
+            AddTail(item);
+        }
+
+        public TValue Dequeue()
+        {
+            return RemoveHead();
+        }
+
+        public TValue Peek()
+        {
+            return GetHead();
+        }
+    }
+}
