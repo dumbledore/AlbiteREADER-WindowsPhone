5d64546bd4538c9f0dadd39b98d2b74f38c01e12
Svetlin Ankov <galileostudios@gmail.com>
2014-01-05 16:25:02 +0200

    Added CircularBufferStack

---
+++ AlbiteCore/Source/Collections/CircularBufferStack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace SvetlinAnkov.Albite.Core.Collections
+{
+    public class CircularBufferStack<TValue> : CircularBuffer<TValue>
+    {
+        public CircularBufferStack(int maximumCapacity)
+            : base(maximumCapacity) { }
+
+        public CircularBufferStack(IEnumerable<TValue> collection)
+            : base(collection) { }
+
+        public void Push(TValue item)
+        {
+            AddHead(item);
+        }
+
+        public TValue Pop()
+        {
+            return RemoveHead();
+        }
+
+        public TValue Peek()
+        {
+            return GetHead();
+        }
+    }
+}
