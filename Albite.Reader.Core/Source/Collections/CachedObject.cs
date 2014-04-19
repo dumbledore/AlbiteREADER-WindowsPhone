18a83caaf944928b8373ff411d796b6cd342ed2e
Svetlin Ankov <galileostudios@gmail.com>
2014-04-19 23:17:29 +0300

    Core: Added CachedObject

---
+++ Albite.Reader.Core/Source/Collections/CachedObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Albite.Reader.Core.Collections
+{
+    public abstract class CachedObject<TValue> where TValue : class
+    {
+        private WeakReference<TValue> cachedValue
+            = new WeakReference<TValue>(null);
+
+        protected abstract TValue GenerateValue();
+
+        public TValue Value
+        {
+            get
+            {
+                TValue value = null;
+
+                lock (cachedValue)
+                {
+                    if (!cachedValue.TryGetTarget(out value))
+                    {
+                        value = GenerateValue();
+                        cachedValue.SetTarget(value);
+                    }
+                }
+
+                return value;
+            }
+        }
+    }
+}
