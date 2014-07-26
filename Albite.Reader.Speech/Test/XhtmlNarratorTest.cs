560ca44311094927b4f0d443369270a9124fd922
Svetlin Ankov <galileostudios@gmail.com>
2014-07-26 22:05:38 +0300

    XHtmlNarratorTest seems to be done now

--- Albite.Reader.Speech/Test/XhtmlNarratorTest.cs
+++ Albite.Reader.Speech/Test/XhtmlNarratorTest.cs
@@ -4,6 +4,8 @@ using Albite.Reader.Speech.Narration;
 using Albite.Reader.Speech.Narration.Xhtml;
 using System;
 using System.IO;
+using System.Linq;
+using Windows.Foundation;
 
 namespace Albite.Reader.Speech.Test
 {
@@ -19,21 +21,43 @@ namespace Albite.Reader.Speech.Test
         protected override async void TestImplementation()
         {
             NarrationSettings settings = new NarrationSettings();
+            IAsyncAction a;
 
             using (ResourceStorage res = new ResourceStorage(FilePath))
             {
                 using (Stream stream = res.GetStream(FileAccess.Read))
                 {
-                    XhtmlNarrator narrator = new XhtmlNarrator(stream, "en", settings);
-                    narrator.LocatedTextManager.TextReached += LocatedTextManager_TextReached;
-                    await narrator.ReadAsync();
+                    using (XhtmlNarrator narrator = new XhtmlNarrator(stream, "en", settings))
+                    {
+
+                        narrator.LocatedTextManager.TextReached += LocatedTextManager_TextReached;
+
+                        Log("Reading 10s of text");
+                        a = narrator.ReadAsync();
+                        System.Threading.Thread.Sleep(10000);
+                        narrator.Stop();
+                        Log("Enough");
+
+                        Log("Now restart the read. Should continue from the last text");
+                        a = narrator.ReadAsync();
+                        System.Threading.Thread.Sleep(5000);
+                        narrator.Stop();
+                        Log("Stop the narrator");
+
+                        Log("Now skip some nodes and read from a later position");
+                        narrator.LocatedTextManager.Current = narrator.LocatedTextManager.Locations.Skip(88).First();
+
+                        // Await completion
+                        await narrator.ReadAsync();
+                        Log("Narration ended");
+                    }
                 }
             }
         }
 
         void LocatedTextManager_TextReached(LocatedTextManager<XhtmlLocation> sender, ILocatedText<XhtmlLocation> args)
         {
-            Log("Reached: {0}", args.Text);
+            Log("Reached #{0}: {1}", args.Id, args.Text);
         }
     }
 }
