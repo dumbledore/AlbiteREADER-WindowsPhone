326635aa0e7c14632650982bcf47034fefe62491
Svetlin Ankov <galileostudios@gmail.com>
2014-07-26 17:53:56 +0300

    Removed unused stuff from Narrator

--- Albite.Reader.Speech/Source/Narration/Narrator.cs
+++ Albite.Reader.Speech/Source/Narration/Narrator.cs
@@ -1,20 +1,12 @@
-using Albite.Reader.Core.Diagnostics;
 using Albite.Reader.Speech.Narration.Elements;
 using Albite.Reader.Speech.Synthesis;
 using Albite.Reader.Speech.Synthesis.Elements;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
 using Windows.Foundation;
-using Windows.Phone.Speech.Synthesis;
 
 namespace Albite.Reader.Speech.Narration
 {
     public abstract class Narrator
     {
-        private static readonly string Tag = "Narrator";
-
         public RootElement Root { get; private set; }
 
         private Synthesizer synth;
