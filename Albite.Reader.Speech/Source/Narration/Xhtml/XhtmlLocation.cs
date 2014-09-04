00030da505c737afa976de08432b7d6612d45899
Svetlin Ankov <galileostudio@gmail.com>
2014-09-04 00:12:46 -0700

    XhtmlLocation: Implement Equals()
    This is needed for proper use by LocatedTextManager

--- Albite.Reader.Speech/Source/Narration/Xhtml/XhtmlLocation.cs
+++ Albite.Reader.Speech/Source/Narration/Xhtml/XhtmlLocation.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Albite.Reader.Speech.Narration.Xhtml
 {
@@ -8,7 +10,28 @@ namespace Albite.Reader.Speech.Narration.Xhtml
 
         public XhtmlLocation(IEnumerable<int> elementPath)
         {
+            if (elementPath == null)
+            {
+                throw new NullReferenceException();
+            }
+
             ElementPath = elementPath;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is XhtmlLocation)
+            {
+                XhtmlLocation other = (XhtmlLocation)obj;
+                return Enumerable.SequenceEqual<int>(ElementPath, other.ElementPath);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ElementPath.GetHashCode();
         }
     }
 }
