7ab6dff5adb400d560e83bbf037928917cad5d1a
Svetlin Ankov <galileostudio@gmail.com>
2014-09-18 23:21:46 -0700

    Moved the code from NarrationPage to NarrationController

---
+++ Albite.Reader.App/Source/NarrationController.cs
@@ -0,0 +1,231 @@
+﻿using Albite.Reader.BookLibrary;
+using Albite.Reader.BookLibrary.Location;
+using Albite.Reader.Core.Diagnostics;
+using Albite.Reader.Core.IO;
+using Albite.Reader.Speech;
+using Albite.Reader.Speech.Narration;
+using Albite.Reader.Speech.Narration.Xhtml;
+using Microsoft.Phone.Shell;
+using System;
+using System.IO;
+using System.Windows.Threading;
+using Windows.Foundation;
+
+namespace Albite.Reader.App
+{
+    public class NarrationController : IDisposable
+    {
+        public event TypedEventHandler<NarrationController, string> UpdateText;
+
+        private XhtmlNarrator narrator;
+        private Chapter chapter;
+        private Dispatcher dispatcher;
+        private string baseLanguage;
+        private bool disposed = false;
+        private object myLock = new object();
+
+        private static readonly string tag = "NarrationController";
+
+        public NarrationController(Dispatcher dispatcher, string baseLanguage)
+        {
+            this.dispatcher = dispatcher;
+            this.baseLanguage = baseLanguage;
+
+            // Now load the narrator. No need to hold a lock as
+            // no text is being read, i.e. no events will occurr.
+            loadNarrator();
+        }
+
+        public void Dispose()
+        {
+            lock (myLock)
+            {
+                throwIfDisposed();
+                unloadNarrator();
+                disposed = true;
+            }
+        }
+
+        public void StartReading()
+        {
+            lock (myLock)
+            {
+                throwIfDisposed();
+                startReadingLocked();
+            }
+        }
+
+        public void StopReading()
+        {
+            lock (myLock)
+            {
+                throwIfDisposed();
+                stopReadingLocked();
+            }
+        }
+
+        public void StopReadingAndPersist()
+        {
+            lock (myLock)
+            {
+                throwIfDisposed();
+
+                // First, stop reading
+                stopReadingLocked();
+
+                // Update the BookLocation of BookPresenter to match the current BookLocation
+                ILocatedText<XhtmlLocation> current = narrator.LocatedTextManager.Current;
+                if (current != null)
+                {
+                    // Relative position is dummy (0), but that shouldn't be an issue as it
+                    // is not going to be used anyway, yet it will be updated in ReaderPage
+                    persistLocaiton(new DomLocation(current.Location.ElementPath, 0, 0));
+                }
+                else
+                {
+                    // Somehow current is null. Still, had beter persist the chapter at least
+                    persistLocaiton(new FirstPageLocation());
+                }
+            }
+        }
+
+
+        private void readingCompleted(IAsyncAction asyncInfo, AsyncStatus asyncStatus)
+        {
+            if (asyncStatus == AsyncStatus.Completed)
+            {
+                // Chapter ended.
+                // Queue this for later or narrator.Dispose() would dead-lock
+                dispatcher.BeginInvoke(() =>
+                {
+                    lock (myLock)
+                    {
+                        throwIfDisposed();
+
+                        // Just in case
+                        stopReadingLocked();
+
+                        // Check if it's the last chapter.
+                        if (chapter.Next != null)
+                        {
+                            // Set to next chapter
+                            chapter = chapter.Next;
+
+                            // And now persist to first page
+                            persistLocaiton(new FirstPageLocation());
+                        }
+
+                        // Current narrator is not needed anymore
+                        unloadNarrator();
+
+                        // Reload new narrator (for the new location)
+                        loadNarrator();
+
+                        // And start
+                        startReadingLocked();
+                    }
+                });
+            }
+        }
+
+        private void startReadingLocked()
+        {
+            narrator.ReadAsync().Completed = readingCompleted;
+
+            // Disable idle detection so that it can run under locked screen
+            PhoneApplicationService.Current.ApplicationIdleDetectionMode = IdleDetectionMode.Disabled;
+        }
+
+        private void stopReadingLocked()
+        {
+            narrator.Stop();
+        }
+
+        private void loadNarrator()
+        {
+            BookPresenter presenter = App.Context.BookPresenter;
+
+            // Now get the latest location
+            BookLocation location = presenter.HistoryStack.GetCurrentLocation();
+
+            // Cache the current chapter
+            chapter = location.Chapter;
+
+            // Get the name of the chapter file
+            string path = Path.Combine(presenter.ContentPath, chapter.Url);
+
+            using (IsolatedStorage iso = new IsolatedStorage(path))
+            {
+                using (Stream stream = iso.GetStream(FileAccess.Read))
+                {
+                    narrator = new XhtmlNarrator(stream, baseLanguage, new NarrationSettings());
+                    narrator.LocatedTextManager.TextReached += textReached;
+                }
+            }
+
+            // Now use the provided location
+            try
+            {
+                if (location.Location is LastPageLocation)
+                {
+                    // go to last text location
+                }
+                else if (location.Location is DomLocation)
+                {
+                    // go to html
+                    DomLocation domLocation = (DomLocation)location.Location;
+                    XhtmlLocation xhtmlLocation = new XhtmlLocation(domLocation.ElementPath);
+                    ILocatedText<XhtmlLocation> locatedText = narrator.LocatedTextManager[xhtmlLocation];
+                    narrator.LocatedTextManager.Current = locatedText;
+                } // all other default to first page - nothing to do
+            }
+            catch (Exception ex)
+            {
+                Log.W(tag, "Failed skipping to the location", ex);
+            }
+
+            if (UpdateText != null)
+            {
+                UpdateText(this, narrator.LocatedTextManager.Current.Text);
+            }
+        }
+
+        private void unloadNarrator()
+        {
+            narrator.Stop();
+            narrator.Dispose();
+            narrator = null;
+
+            // Do not leak Chapter/BookPresenter
+            chapter = null;
+        }
+
+        private void persistLocaiton(ChapterLocation cLocation)
+        {
+            // Create the book location
+            BookLocation bLocation = chapter.CreateLocation(cLocation);
+
+            // Update the location in HistoryStack
+            chapter.BookPresenter.HistoryStack.SetCurrentLocation(bLocation);
+
+            // And finally persist
+            chapter.BookPresenter.Persist();
+        }
+
+        private void textReached(LocatedTextManager<XhtmlLocation> sender, ILocatedText<XhtmlLocation> args)
+        {
+            if (UpdateText != null)
+            {
+                UpdateText(this, args.Text);
+            }
+        }
+
+        private void throwIfDisposed()
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException(GetType().FullName);
+            }
+        }
+    }
+}
--- Albite.Reader.App/Source/View/Pages/NarrationPage.xaml.cs
+++ Albite.Reader.App/Source/View/Pages/NarrationPage.xaml.cs
@@ -1,28 +1,13 @@
 ﻿using Microsoft.Phone.Controls;
 using Albite.Reader.BookLibrary;
 using System.Windows.Navigation;
-using Albite.Reader.Speech.Narration.Xhtml;
-using Albite.Reader.BookLibrary.Location;
-using System.IO;
-using Albite.Reader.Core.IO;
-using Albite.Reader.Speech.Narration;
-using Albite.Reader.Speech;
-using System.Linq;
-using System;
-using Albite.Reader.Core.Diagnostics;
 using System.Globalization;
-using Windows.Foundation;
-using Microsoft.Phone.Shell;
 
 namespace Albite.Reader.App.View.Pages
 {
     public partial class NarrationPage : PhoneApplicationPage
     {
-        private static readonly string tag = "NarrationPage";
-
-        private XhtmlNarrator narrator;
-        private Chapter chapter;
-        private object myLock = new object();
+        private NarrationController narrationController;
 
         public NarrationPage()
         {
@@ -38,207 +23,43 @@ namespace Albite.Reader.App.View.Pages
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            lock (myLock)
+            if (NavigationContext.QueryString.ContainsKey("id"))
             {
-                loadBookPresenter();
-                startReading();
+                int bookId = int.Parse(NavigationContext.QueryString["id"]);
+                App.Context.OpenBook(bookId);
             }
+
+            // Create a new controller
+            narrationController = new NarrationController(Dispatcher, CultureInfo.CurrentCulture.Name);
+
+            // Set up text updates
+            narrationController.UpdateText += updateText;
+
+            // And start reading
+            narrationController.StartReading();
 
             base.OnNavigatedTo(e);
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
-            lock (myLock)
-            {
-                // stop the narrator
-                stopReading();
+            // Persist the current position
+            narrationController.StopReadingAndPersist();
 
-                if (chapter != null)
-                {
-                    // Update the BookLocation of BookPresenter to match
-                    // the current BookLocation
-                    ILocatedText<XhtmlLocation> current = narrator.LocatedTextManager.Current;
-                    if (current != null)
-                    {
-                        // Relative position is dummy (0), but that shouldn't be an issue as it
-                        // is not going to be used anyway, yet it will be updated in ReaderPage
-                        persistLocaiton(new DomLocation(current.Location.ElementPath, 0, 0));
-                    }
+            // Free resources
+            narrationController.Dispose();
 
-                    // Do not leak the Chapter/BookPresenter
-                    chapter = null;
-                }
-
-                // Going away. No need to keep the resources
-                if (e.NavigationMode == NavigationMode.Back)
-                {
-                    unloadNarrator();
-                }
-            }
+            // We don't need the controller anymore
+            narrationController = null;
 
             base.OnNavigatedFrom(e);
         }
 
-        // shall be called locked
-        private void persistLocaiton(ChapterLocation cLocation)
+        void updateText(NarrationController sender, string args)
         {
-            // Create the book location
-            BookLocation bLocation = chapter.CreateLocation(cLocation);
-
-            // Update the location in HistoryStack
-            chapter.BookPresenter.HistoryStack.SetCurrentLocation(bLocation);
-
-            // And finally persist
-            chapter.BookPresenter.Persist();
-        }
-
-        // shall be called locked
-        private void loadBookPresenter()
-        {
-            if (narrator == null)
+            Dispatcher.BeginInvoke(() =>
             {
-                // Either page opened for the first time,
-                // or app has been tombstoned. In either
-                // case we need to load the narrator.
-
-                // First get the BookPresenter
-                if (NavigationContext.QueryString.ContainsKey("id"))
-                {
-                    int bookId = int.Parse(NavigationContext.QueryString["id"]);
-                    App.Context.OpenBook(bookId);
-                }
-
-                loadNarrator();
-            }
-        }
-
-        // shall be called locked
-        private void loadNarrator()
-        {
-            if (narrator == null)
-            {
-                BookPresenter presenter = App.Context.BookPresenter;
-
-                // Now get the latest location
-                BookLocation location = presenter.HistoryStack.GetCurrentLocation();
-
-                // Cache the current chapter
-                chapter = location.Chapter;
-
-                // Get the name of the chapter file
-                string path = Path.Combine(presenter.ContentPath, chapter.Url);
-
-                using (IsolatedStorage iso = new IsolatedStorage(path))
-                {
-                    using (Stream stream = iso.GetStream(FileAccess.Read))
-                    {
-                        narrator = new XhtmlNarrator(stream, CultureInfo.CurrentCulture.Name, new NarrationSettings());
-                        narrator.LocatedTextManager.TextReached += LocatedTextManager_TextReached;
-                    }
-                }
-
-                // Now use the provided location
-                try
-                {
-                    if (location.Location is LastPageLocation)
-                    {
-                        // go to last text location
-                    }
-                    else if (location.Location is DomLocation)
-                    {
-                        // go to html
-                        DomLocation domLocation = (DomLocation)location.Location;
-                        XhtmlLocation xhtmlLocation = new XhtmlLocation(domLocation.ElementPath);
-                        ILocatedText<XhtmlLocation> locatedText = narrator.LocatedTextManager[xhtmlLocation];
-                        narrator.LocatedTextManager.Current = locatedText;
-                    } // all other default to first page - nothing to do
-                }
-                catch (Exception ex)
-                {
-                    Log.W(tag, "Failed skipping to the location", ex);
-                }
-                NarrationBlock.Text = narrator.LocatedTextManager.Current.Text;
-            }
-        }
-
-        // shall be called locked
-        private void unloadNarrator()
-        {
-            if (narrator != null)
-            {
-                narrator.Stop();
-                narrator.Dispose();
-                narrator = null;
-            }
-
-            // Do not leak Chapter/BookPresenter
-            chapter = null;
-        }
-
-        // shall be called locked
-        private void startReading()
-        {
-            if (narrator != null)
-            {
-                narrator.ReadAsync().Completed = readingCompleted;
-
-                // Disable idle detection so that it can run under locked screen
-                PhoneApplicationService.Current.ApplicationIdleDetectionMode = IdleDetectionMode.Disabled;
-            }
-        }
-
-        // shall be called locked
-        private void stopReading()
-        {
-            if (narrator != null)
-            {
-                narrator.Stop();
-            }
-        }
-
-        private void readingCompleted(IAsyncAction asyncInfo, AsyncStatus asyncStatus)
-        {
-            if (asyncStatus == AsyncStatus.Completed)
-            {
-                // Chapter ended.
-                // Queue this for later or narrator.Dispose() would dead-lock
-                Dispatcher.BeginInvoke(() =>
-                    {
-                        lock (myLock)
-                        {
-                            // Just in case
-                            stopReading();
-
-                            // Chapter shan't be null, but just in case.
-                            // Also, check if it's the last chapter.
-                            if (chapter != null && chapter.Next != null)
-                            {
-                                // Set to next chapter
-                                chapter = chapter.Next;
-
-                                // And now persist to first page
-                                persistLocaiton(new FirstPageLocation());
-                            }
-
-                            // Current narrator is not needed anymore
-                            unloadNarrator();
-
-                            // Reload new narrator (for the new location)
-                            loadNarrator();
-
-                            // And start
-                            startReading();
-                        }
-                    });
-            }
-        }
-
-        private void LocatedTextManager_TextReached(LocatedTextManager<XhtmlLocation> sender, ILocatedText<XhtmlLocation> args)
-        {
-            NarrationBlock.Dispatcher.BeginInvoke(() =>
-            {
-                NarrationBlock.Text = args.Text;
+                NarrationBlock.Text = args;
             });
         }
     }
