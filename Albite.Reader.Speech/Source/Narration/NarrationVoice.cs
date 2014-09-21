8fad449a277bf0627189a2e4540e92912cdc521a
Svetlin Ankov <galileostudio@gmail.com>
2014-09-20 17:14:45 -0700

    Speech: Added NarrationVoice

---
+++ Albite.Reader.Speech/Source/Narration/NarrationVoice.cs
@@ -0,0 +1,26 @@
+﻿using Windows.Phone.Speech.Synthesis;
+
+namespace Albite.Reader.Speech.Narration
+{
+    public class NarrationVoice
+    {
+        public string Language { get; private set; }
+
+        public bool Male { get; private set; }
+
+        public NarrationVoice(string language, bool male)
+        {
+            Language = language;
+            Male = male;
+        }
+
+        public static NarrationVoice Default
+        {
+            get
+            {
+                VoiceInformation v = InstalledVoices.Default;
+                return new NarrationVoice(v.Language, v.Gender == VoiceGender.Male);
+            }
+        }
+    }
+}
