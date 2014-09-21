f3a2186549306611f91201b54e470e2e75ae06e1
Svetlin Ankov <galileostudio@gmail.com>
2014-09-20 21:06:41 -0700

    App: Abstracted ThemeControl into SelectableControl

---
+++ Albite.Reader.App/Source/View/Controls/SelectableControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Albite.Reader.App.View.Controls
+{
+    public partial class SelectableControl : UserControl
+    {
+        public SelectableControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(SelectableControl),
+            new PropertyMetadata(onTextChanged));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private static void onTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SelectableControl control = (SelectableControl)d;
+            string newValue = (string)e.NewValue;
+            control.ForegroundText.Text = newValue;
+        }
+
+        public static readonly DependencyProperty SelectedProperty =
+            DependencyProperty.Register("Selected", typeof(bool), typeof(SelectableControl),
+            new PropertyMetadata(onSelectedChanged));
+
+        public bool Selected
+        {
+            get { return (bool)GetValue(SelectedProperty); }
+            set { SetValue(SelectedProperty, value); }
+        }
+
+        public static void onSelectedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SelectableControl control = (SelectableControl)d;
+            bool newValue = (bool)e.NewValue;
+            Brush brush = null;
+            if (newValue)
+            {
+                brush = (Brush)Application.Current.Resources["PhoneAccentBrush"];
+            }
+
+            control.SelectionBorder.Background = brush;
+        }
+    }
+}
---
+++ Albite.Reader.App/Source/View/Controls/ThemeControl.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Albite.Reader.App.View.Controls
+{
+    public class ThemeControl : SelectableControl
+    {
+        public static readonly DependencyProperty BackgroundColorProperty =
+            DependencyProperty.Register("BackgroundColor", typeof(Color), typeof(SelectableControl),
+            new PropertyMetadata(onBackgroundColorChanged));
+
+        public Color BackgroundColor
+        {
+            get { return (Color)GetValue(BackgroundColorProperty); }
+            set { SetValue(BackgroundColorProperty, value); }
+        }
+
+        public static void onBackgroundColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SelectableControl control = (SelectableControl)d;
+            Color newValue = (Color)e.NewValue;
+            control.BackgroundBorder.Background = new SolidColorBrush(newValue);
+        }
+
+        public static readonly DependencyProperty ForegroundColorProperty =
+            DependencyProperty.Register("ForegroundColor", typeof(Color), typeof(SelectableControl),
+            new PropertyMetadata(onForegroundColorChanged));
+
+        public Color ForegroundColor
+        {
+            get { return (Color)GetValue(ForegroundColorProperty); }
+            set { SetValue(ForegroundColorProperty, value); }
+        }
+
+        public static void onForegroundColorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SelectableControl control = (SelectableControl)d;
+            Color newValue = (Color)e.NewValue;
+            SolidColorBrush colorBrush = new SolidColorBrush(newValue);
+            control.ForegroundText.Foreground = colorBrush;
+            control.BackgroundBorder.BorderBrush = colorBrush;
+        }
+
+        public new static readonly DependencyProperty FontFamilyProperty =
+            DependencyProperty.Register("FontFamily", typeof(FontFamily), typeof(SelectableControl),
+            new PropertyMetadata(onFontFamilyChanged));
+
+        public new FontFamily FontFamily
+        {
+            get { return (FontFamily)GetValue(FontFamilyProperty); }
+            set { SetValue(FontFamilyProperty, value); }
+        }
+
+        public static void onFontFamilyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SelectableControl control = (SelectableControl)d;
+            FontFamily family = (FontFamily)e.NewValue;
+            control.ForegroundText.FontFamily = family;
+        }
+
+        // Font size is in logical pixels.
+        // See http://www.silverlightshow.net/items/Windows-Phone-8-Multiple-Screen-Resolutions.aspx
+        public new static readonly DependencyProperty FontSizeProperty =
+            DependencyProperty.Register("FontSize", typeof(double), typeof(SelectableControl),
+            new PropertyMetadata(onFontSizeChanged));
+
+        public new double FontSize
+        {
+            get { return (double)GetValue(FontSizeProperty); }
+            set { SetValue(FontSizeProperty, value); }
+        }
+
+        public static void onFontSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SelectableControl control = (SelectableControl)d;
+            double fontSize = (double)e.NewValue;
+            control.ForegroundText.FontSize = fontSize;
+        }
+
+        public static readonly DependencyProperty LineSpacingProperty =
+            DependencyProperty.Register("LineSpacing", typeof(double), typeof(SelectableControl),
+            new PropertyMetadata(null));
+
+        public double LineSpacing
+        {
+            get { return (double)GetValue(LineSpacingProperty); }
+            set { SetValue(LineSpacingProperty, value); }
+        }
+
+        public static readonly DependencyProperty PageMarginsProperty =
+            DependencyProperty.Register("PageMargins", typeof(double), typeof(SelectableControl),
+            new PropertyMetadata(null));
+
+        public double PageMargins
+        {
+            get { return (double)GetValue(PageMarginsProperty); }
+            set { SetValue(PageMarginsProperty, value); }
+        }
+    }
+}
