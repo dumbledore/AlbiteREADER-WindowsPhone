18f691809c38f14a691cc24746e9ceedcbf1757e
Svetlin Ankov <galileostudio@gmail.com>
2014-09-21 20:11:15 -0700

    App: Implemented the Microphone button for VoiceSearchControl

--- Albite.Reader.App/Source/View/Controls/VoiceSearchControl.xaml.cs
+++ Albite.Reader.App/Source/View/Controls/VoiceSearchControl.xaml.cs
@@ -1,11 +1,14 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using Windows.Foundation;
 using System.Windows.Input;
+using Windows.Phone.Speech.Recognition;
+using Albite.Reader.Core.Collections;
 
 namespace Albite.Reader.App.View.Controls
 {
-    public partial class VoiceSearchControl : UserControl
+    public partial class VoiceSearchControl : UserControl, IDisposable
     {
         public VoiceSearchControl()
         {
@@ -13,6 +16,8 @@ namespace Albite.Reader.App.View.Controls
         }
 
         public event TypedEventHandler<VoiceSearchControl, string> SearchInitiated;
+
+        private readonly CachedRecognizer cachedRecognizer = new CachedRecognizer();
 
         public void Show()
         {
@@ -35,19 +40,60 @@ namespace Albite.Reader.App.View.Controls
         {
             if (e.Key == Key.Enter)
             {
-                // Initiate a search
+                // Get the textbox
                 TextBox box = (TextBox)sender;
 
-                if (SearchInitiated != null)
-                {
-                    SearchInitiated(this, box.Text);
-                }
+                // Initiate a search
+                initiateSearch(box.Text);
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            SpeechRecognizerUI recognizer = cachedRecognizer.Value;
+            SpeechRecognitionUIResult result = await recognizer.RecognizeWithUIAsync();
 
+            if (result.ResultStatus == SpeechRecognitionUIStatus.Succeeded)
+            {
+                string searchText = result.RecognitionResult.Text;
+
+                // Remove the annoying period
+                if (searchText.EndsWith(".") && searchText.Length > 1)
+                {
+                    searchText = searchText.Substring(0, searchText.Length - 1);
+                }
+
+                // Update text box
+                SearchBox.Text = searchText;
+
+                // Initiate search
+                initiateSearch(searchText);
+            }
+        }
+
+        private void initiateSearch(string searchString)
+        {
+            if (SearchInitiated != null)
+            {
+                SearchInitiated(this, searchString);
+            }
+        }
+
+        private class CachedRecognizer : DisposableCachedObject<SpeechRecognizerUI>
+        {
+            protected override SpeechRecognizerUI GenerateValue()
+            {
+                SpeechRecognizerUI recognizer = new SpeechRecognizerUI();
+                recognizer.Settings.ExampleText = "Adventures of Sherlock Holmes";
+                recognizer.Settings.ReadoutEnabled = true;
+                recognizer.Settings.ShowConfirmation = true;
+                return recognizer;
+            }
+        }
+
+        public void Dispose()
+        {
+            cachedRecognizer.Dispose();
         }
     }
 }
